1b04c3cf78d86e8a44ac012084de2493b78253b1
Lion <lionkiller616@gmail.com>
2025-03-18 04:54:08 -0700

    del

--- r/script.js
+++
@@ -1,9 +0,0 @@
-function toggleRule(element) {
-    let details = element.querySelector(".rule-details");
-    if (details.style.display === "block") {
-      details.style.display = "none";
-    } else {
-      details.style.display = "block";
-    }
-  }
-  
