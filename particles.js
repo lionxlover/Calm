eb2528d3b5f369785eda7245659406f6dfe0799f
Lion <lionkiller616@gmail.com>
2025-03-18 15:32:40 +0530

    Create particles.js

---
+++ particles.js
@@ -0,0 +1,10 @@
+{
+  "particles": {
+    "number": { "value": 100 },
+    "color": { "value": "#ffffff" },
+    "shape": { "type": "circle" },
+    "size": { "value": 3 },
+    "opacity": { "value": 0.5 },
+    "move": { "speed": 2 }
+  }
+}
